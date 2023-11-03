85a9d76ce6c902ef42aba95aad7a09e0f6f59f83
John Santana Zorrilla <johnsantanahe@hotmail.com>
2023-11-02 20:21:43 -0400

    Added Response Status Exception Library

--- geekTest/src/main/java/com/company/geekTest/controller/CustomerController.java
+++ geekTest/src/main/java/com/company/geekTest/controller/CustomerController.java
@@ -5,6 +5,7 @@ import com.company.geekTest.repository.CustomerRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
 
 import java.util.List;
 
