8b50c93d64afcd42505d400e2959bd3e0b8a5688
csaou001 <133934179+csaou001@users.noreply.github.com>
2023-11-06 18:59:56 -0500

    updated code

---
+++ geekTest/src/main/java/com/company/geekTest/repository/WishlistRepository.java
@@ -0,0 +1,10 @@
+package com.company.geekTest.repository;
+
+import com.company.geekTest.model.Wishlist;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface WishlistRepository extends JpaRepository<Wishlist, Integer> {
+   
+}
